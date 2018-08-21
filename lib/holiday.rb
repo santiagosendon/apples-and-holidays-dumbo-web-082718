62c7dfbcba5b1b78d7f7e919f142b09bd3405708
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-21 00:38:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
     }
   }
  
-  holiday_hash[:summer][:fourth_of_july][1]
+  return holiday_hash[:summer][:fourth_of_july][1]
   # return the second element in the 4th of July array
 end
 
